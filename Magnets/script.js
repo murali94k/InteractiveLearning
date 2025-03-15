8e092006fd813f778583bb99c735b164fe3cd3df
Murali G <murali94k@gmail.com>
2025-03-16 00:17:10 +0530

    riveDemo

---
+++ Magnets/script.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+
+const resetButtonElement = document.getElementById('reset-btn');
+const newButtonElement = document.getElementById('new-btn');
+
+
+let canvasPosition = canvas.getBoundingClientRect();
+
+
+const canvasWidth =   window.innerHeight/1.5;
+const canvasHeight =  window.innerHeight/1.5;
+
+canvas.width = canvasWidth; //600 px
+canvas.height = canvasHeight; // 300 px
+
+const r = new rive.Rive({
+        src: "./Assets/nail.riv",
+        // OR the path to a discoverable and public Rive asset
+        // src: '/public/example.riv',
+        canvas: document.getElementById("canvas1"),
+        autoplay: true,
+        // artboard: "Arboard", // Optional. If not supplied the default is selected
+        stateMachines: "State Machine 1",
+        onLoad: () => {
+          r.resizeDrawingSurfaceToCanvas();
+        },
+    });
